4beb48896f13e50257509248c042a298606ba60d
Richard-Ochayi <ochayirichkid@gmail.com>
2025-07-01 14:25:12 +0100

    fixed google isssue 3

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,5 @@
 "use client"
 import LoginForm from "@/components/LoginForm";
-import { Button } from "@/components/ui/button";
-import { FormField } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { signIn } from "next-auth/react";
 import React, { FormEvent } from "react";
 
 export default function Login() {
